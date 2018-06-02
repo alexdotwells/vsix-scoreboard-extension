8b987351b239e08acbf454c5512b661499465f76
AWells <artoo@ARTOO-D2>
2018-06-02 01:16:26 -0500

    Added library clases for Team and DailyScores

---
+++ VsixScoreboardExtension/VsixScoreboardExtension/Library/DailyScores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsixScoreboardExtension
+{
+    class DailyScores
+    {
+        public DateTime Date = DateTime.Now.Date;
+        public string Season { get; set; }
+        private List<string> CachedSeasons { get; set; }
+    }
+}
---
+++ VsixScoreboardExtension/VsixScoreboardExtension/Library/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsixScoreboardExtension
+{
+    class Team
+    {
+        public string Name { get; private set; }
+
+        public Team(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
