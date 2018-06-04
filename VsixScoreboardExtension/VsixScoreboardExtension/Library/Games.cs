36eb4678c9db2243045455c9138a5404ee7688f6
AWells <artoo@ARTOO-D2>
2018-06-03 23:33:35 -0500

    Added Games class (inital commit)

---
+++ VsixScoreboardExtension/VsixScoreboardExtension/Library/Games.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsixScoreboardExtension.Library
+{
+    class Games
+    {
+        public Teams HomeTeam { get; set; }
+        public Teams AwayTeam { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+    }
+}
