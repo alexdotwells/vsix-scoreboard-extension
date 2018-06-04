c0ddca37f8d24348a3bcca83952326c63e71e24c
AWells <artoo@ARTOO-D2>
2018-06-03 23:34:33 -0500

    Added Location to Teams class

--- VsixScoreboardExtension/VsixScoreboardExtension/Library/Teams.cs
+++ VsixScoreboardExtension/VsixScoreboardExtension/Library/Teams.cs
@@ -9,10 +9,12 @@ namespace VsixScoreboardExtension
     class Teams
     {
         public string Name { get; private set; }
+        public string Location { get; set; }
 
-        public Teams(string name)
+        public Teams(string name, string location)
         {
             this.Name = name;
+            this.Location = location;
         }
     }
 }
